a494d9d129ed4a9a4088a3133e323941bacd680b
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2021-03-19 13:49:04 -0700

    add analytics. cuz why not?

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,14 @@
       <h1>One Click DB</h1>      
       <p>lorem ipsum dolor words, words, and more. lorem ipsum dolor words, words, and more. lorem ipsum dolor words, words, and more. lorem ipsum dolor words, words, and more. lorem ipsum dolor words, words, and more. lorem ipsum dolor words, words, and more. lorem ipsum dolor words, words, and more. </p>
 
+      <h2>Wanna race?</h2>
+      <p>One Click DB is already awesome. And it’s about to get even awesomer (while staying simple).</p>
+      <p>Here's a list features coming in Spring 2021.</p>
+      <p>Note that the early-bird plan will <strong>disappear forever</strong> once this list is completed. So.. what’re you waiting for 😬</p>
+      <ul>
+        <li><strong>Deploy hook:</strong></li>
+      </ul>
+
       <nuxt-link to="/home">Login</nuxt-link>
     </div>    
   </div>
--- plugins/packages.js
+++ plugins/packages.js
@@ -3,4 +3,6 @@ import VueCookie from 'vue-cookie'
 Vue.use(VueCookie);
 
 import splitbee from '@splitbee/web';
-splitbee.init();
+splitbee.init({
+    token: process.env.SPLITBEE_TOKEN
+});
