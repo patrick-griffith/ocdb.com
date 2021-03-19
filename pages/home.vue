339a65578b080eef5e7239138b267f377e52b10e
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2021-03-19 13:26:21 -0700

    add ability to logout

--- pages/home.vue
+++ pages/home.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         this is a logged in page.
+        <span @click="logout" class="link">Logout --></span>
     </div>
 </template>
 <script>
 export default {
     middleware: ['auth', 'emailConfirmed'],    
+    methods: {        
+        async logout() {
+            await this.$auth.logout();
+        }
+    },
 }
 </script>
