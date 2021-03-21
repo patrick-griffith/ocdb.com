27cab7f282f39062bca2f39c92eb3e1ce90080ef
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2021-03-21 04:31:38 -0700

    add horrible favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,11 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/img/fav/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/img/fav/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/img/fav/favicon-16x16.png" },
+      { rel: "manifest", href: "/img/fav/site.webmanifest" },
+      { rel: "mask-icon", href: "/img/fav/safari-pinned-tab.svg", color: "#1d4ed8" }
     ]
   },
 
